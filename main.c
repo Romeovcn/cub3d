aeb4bbf46e257c58aade142f80f9928ce62bff5d
Romeo VINCENT <rvincent@e2r2p18.clusters.42paris.fr>
2023-02-13 20:25:17 +0100

    working for cos

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 const int MAP_WIDTH = 10;
 const int MAP_HEIGHT = 10;
 
-int map[MAP_HEIGHT][MAP_WIDTH] = {
+int map[10][10] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -17,18 +17,33 @@ int map[MAP_HEIGHT][MAP_WIDTH] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-void get_next1_point(double *x, double *y, double yRatio)
+void get_next1_point(double *x, double *y, double yRatio, int quartile)
 {
-	*x = *x + 1;
-	*y = *y + yRatio;
-	printf("checked in 1 x=%f xIndex=%d y=%f yIndex=%d mapValue=%d ", *x, (int)*x, *y, (int)*y, map[(int)*y][(int)*x]);
+	if (quartile == 1 || quartile == 4)
+		*x = *x + 1;
+	else
+		*x = *x - 1;
+
+	if (quartile == 1 || quartile == 4)
+		*y = *y + yRatio;
+	else if (quartile == 2)
+		*y = *y + (yRatio * -1);
+	else
+		*y = *y - yRatio;
+	printf("checked in 2 x=%f y=%f xIndex=%d yIndex=%d mapValue=%d ", *x, *y, (int)*x, (int)*y, map[(int)*y][(int)*x]);
 }
 
-void get_next2_point(double *x, double *y, double xRatio)
+void get_next2_point(double *x, double *y, double xRatio, int quartile)
 {
-	*x = *x + xRatio;
-	*y = *y + 1;
-	printf("checked in 2 x=%f xIndex=%d y=%f yIndex=%d mapValue=%d ", *x, (int)*x, *y, (int)*y, map[(int)*y][(int)*x]);
+	if (quartile == 1 || quartile == 4)
+		*x = *x + xRatio;
+	else
+		*x = *x - xRatio;
+	if (quartile == 1 || quartile == 2)
+		*y = *y + 1;
+	else
+		*y = *y - 1;
+	printf("checked in 2 x=%f y=%f xIndex=%d yIndex=%d mapValue=%d ", *x, *y, (int)*x, (int)*y, map[(int)*y][(int)*x]);
 }
 
 double get_vector_distance(double playerX, double playerY, double angle) {
@@ -37,57 +52,76 @@ double get_vector_distance(double playerX, double playerY, double angle) {
 	double xRatio = (1 / yDiff) * xDiff;
 	double yRatio = (1 / xDiff) * yDiff;
 
-	//printf("xDiff=%f yDiff=%f\n", xDiff, yDiff);
-	//printf("xRatio=%f yRatio=%f\n", xRatio, yRatio);
+	// printf("xDiff=%f yDiff=%f\n", xDiff, yDiff);
+	// printf("xRatio=%f yRatio=%f\n", xRatio, yRatio);
     double x = playerX;
     double y = playerY;
 	double length = 0;
 
     double x2 = playerX;
     double y2 = playerY;
-	double length2 = 0;
+	int quartile;
 
-	get_next2_point(&x2, &y2, xRatio);
-	length2 = sqrt((y2 - playerY) * (y2 - playerY) + (x2 - playerY) * (x2 - playerY));
-	printf("length=%f\n", length2);
-	if (map[(int)y2][(int)x2] == 1 && x2 >= 0 && x2 < MAP_WIDTH && y2 >= 0 && y2 < MAP_HEIGHT)
+	double length2 = 0.1;
+	if (angle >= 0 && angle <= 90)
 	{
-		printf("TROUVE 2 final length=%f\n", length2);
-		return length2;
+		printf("%f bas droite x++ y++\n", angle);
+		quartile = 1;
 	}
-	get_next1_point(&x, &y, yRatio);
-	length = sqrt((y - playerY) * (y - playerY) + (x - playerY) * (x - playerY));
-	printf("length=%f\n", length);
-	if (map[(int)y][(int)x] == 1)
+	else if (angle >= 91 && angle <= 180)
 	{
-		printf("TROUVE 1 final length=%f\n", length);
-		return length;
+		printf("%f bas gauche x-- y++\n", angle);
+		quartile = 2;
 	}
-	
-    while ((x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT))
+	else if (angle >= 181 && angle <= 270)
+	{
+		printf("%f haut gauche x-- y--\n", angle);
+		quartile = 3;
+	}
+	else if (angle >= 271 && angle <= 360)
+	{
+		printf("%f haut droite x++ y--\n", angle);
+		quartile = 4;
+	}
+    while (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT)
     {
-		if (length > length2)
-		{
-			get_next2_point(&x2, &y2, xRatio);
-			length2 = sqrt((y2 - playerY) * (y2 - playerY) + (x2 - playerY) * (x2 - playerY));
-			printf("length=%f\n", length2);
-			if (map[(int)y2][(int)x2] == 1 && x2 >= 0 && x2 < MAP_WIDTH && y2 >= 0 && y2 < MAP_HEIGHT)
-			{
-				printf("TROUVE 2 final length=%f\n", length2);
-				return length2;
-			}
-		}
-		else
-		{
-			get_next1_point(&x, &y, yRatio);
+		// if (length > length2)
+		// {
+		// 	get_next2_point(&x2, &y2, xRatio, quartile);
+		// 	length2 = sqrt((y2 - playerY) * (y2 - playerY) + (x2 - playerY) * (x2 - playerY));
+		// 	printf("length=%f\n", length2);
+		// 	if (map[(int)y2][(int)x2] == 1 && x2 >= 0 && x2 < MAP_WIDTH && y2 >= 0 && y2 < MAP_HEIGHT)
+		// 	{
+		// 		printf("-------------------\nTROUVE 2 final length=%f at %f, %f\n", length2, x2, y2);
+		// 		return length2;
+		// 	}
+		// }
+		// else
+		// {
+			get_next1_point(&x, &y, yRatio, quartile);
 			length = sqrt((y - playerY) * (y - playerY) + (x - playerY) * (x - playerY));
 			printf("length=%f\n", length);
-			if (map[(int)y][(int)x] == 1)
+			if (quartile == 1 && map[(int)y][(int)x] == 1)
 			{
-				printf("TROUVE 1 final length=%f\n", length);
+				printf("-------------------\nTROUVE 1 final length=%f at %f, %f\n", length, x, y);
 				return length;
 			}
-		}
+			else if (quartile == 2 && map[(int)y][(int)x - 1] == 1)
+			{
+				printf("-------------------\nTROUVE 1 final length=%f at %f, %f\n", length, x, y);
+				return length;
+			}
+			else if (quartile == 3 && map[(int)y][(int)x - 1] == 1)
+			{
+				printf("-------------------\nTROUVE 1 final length=%f at %f, %f\n", length, x, y);
+				return length;
+			}
+			else if (quartile == 4 && map[(int)y][(int)x] == 1)
+			{
+				printf("-------------------\nTROUVE 1 final length=%f at %f, %f\n", length, x, y);
+				return length;
+			}
+		// }
     }
 	return 0;
 }
@@ -96,10 +130,10 @@ int main(void)
 {
     double playerX = 5.0;
     double playerY = 5.0;
-	double angle = 20;
+	double angle = 212.5;
    
-    //for (double angle = 0; angle < 180; angle += 0.1) {
+    // for (double angle = 0; angle < 361; angle += 1) {
 		get_vector_distance(playerX, playerY, angle);
-    //}
+    // }
     return 0;
 }
