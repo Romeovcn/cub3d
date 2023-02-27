6c9abe2bf9652f98d12c9b44c54feca948590d96
Romeovcn <vincent-romeo@hotmail.fr>
2023-02-27 11:16:50 +0100

    refacto

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ const int MAP_HEIGHT = 10;
 int map[10][10] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-    {1, 0, 1, 0, 0, 0, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 0, 1, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
     {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
@@ -17,34 +17,58 @@ int map[10][10] = {
     {1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
 
-void get_next1_point(double *x, double *y, double yRatio, int quartile)
+void get_next_vertical_intersec(double *x, double *y, double y_scale, int quartile)
 {
-	// printf("ratio=%f\n", yRatio);
 	if (quartile == 1 || quartile == 4)
 		*x = *x + 1;
 	else
 		*x = *x - 1;
-
 	if (quartile == 1 || quartile == 4)
-		*y = *y + yRatio;
+		*y = *y + y_scale;
 	else if (quartile == 2)
-		*y = *y + (yRatio * -1);
+		*y = *y + (y_scale * -1);
 	else
-		*y = *y - yRatio;
-	printf("checked in 1 x=%f y=%f xIndex=%d yIndex=%d\n", *x, *y, (int)*x, (int)*y);
+		*y = *y - y_scale;
+	// printf("checked in 1 x=%f y=%f xIndex=%d yIndex=%d\n", *x, *y, (int)*x, (int)*y);
 }
 
-void get_next2_point(double *x, double *y, double xRatio, int quartile)
+int check_map_vertical(int x_vertical, int y_vertical, int quartile)
 {
-	// printf("ratio=%f\n", xRatio);
+	if (x_vertical < 0 || x_vertical > MAP_WIDTH || y_vertical < 0 || y_vertical > MAP_HEIGHT)
+		return (1);
 	if (quartile == 1)
-		*x = *x + xRatio;
+	{
+		if (map[(int)y_vertical][(int)x_vertical] == 1)
+			return (1);
+	}
 	else if (quartile == 2)
-		*x = *x + xRatio;
+	{
+		if (map[(int)y_vertical][(int)x_vertical - 1] == 1)
+			return (1);
+	}
 	else if (quartile == 3)
-		*x = *x - xRatio;
+	{
+		if (map[(int)y_vertical][(int)x_vertical - 1] == 1)
+			return (1);
+	}
+	else if (quartile == 4)
+	{
+		if (map[(int)y_vertical][(int)x_vertical] == 1)
+			return (1);
+	}
+	return (0);
+}
+
+void get_next_horizontal_intersec(double *x, double *y, double x_scale, int quartile)
+{
+	if (quartile == 1)
+		*x = *x + x_scale;
+	else if (quartile == 2)
+		*x = *x + x_scale;
+	else if (quartile == 3)
+		*x = *x - x_scale;
 	else
-		*x = *x + -xRatio;
+		*x = *x + -x_scale;
 	if (quartile == 1 || quartile == 2)
 		*y = *y + 1;
 	else
@@ -52,125 +76,97 @@ void get_next2_point(double *x, double *y, double xRatio, int quartile)
 	// printf("checked in 2 x=%f y=%f xIndex=%d yIndex=%d\n", *x, *y, (int)*x, (int)*y);
 }
 
-double ft_ceil(double nbr)
+int check_map_horizontal(int x_horizontal, int y_horizontal, int quartile)
 {
-	printf("%f\n", (nbr + 1) - nbr);
+	if (x_horizontal < 0 || x_horizontal > MAP_WIDTH || y_horizontal < 0 || y_horizontal > MAP_HEIGHT)
+		return (1);
+	if (quartile == 1)
+	{
+		if (map[(int)y_horizontal][(int)x_horizontal] == 1)
+			return (1);
+	}
+	else if (quartile == 2)
+	{
+		if (map[(int)y_horizontal][(int)x_horizontal] == 1)
+			return (1);
+	}
+	else if (quartile == 3)
+	{
+		if (map[(int)y_horizontal - 1][(int)ceil(x_horizontal) - 1] == 1)
+			return (1);
+	}
+	else if (quartile == 4)
+	{
+		if (map[(int)y_horizontal - 1][(int)x_horizontal] == 1)
+			return (1);
+	}
+	return (0);
 }
 
 double get_vector_distance(double playerX, double playerY, double angle) {
-    double xDiff = cos(angle * ( M_PI / 180));
-    double yDiff = sin(angle * (M_PI / 180));
-	double xRatio = (1 / yDiff) * xDiff;
-	double yRatio = (1 / xDiff) * yDiff;
+    double x_cos = cos(angle * ( M_PI / 180));
+    double y_sin = sin(angle * (M_PI / 180));
+	double x_scale = (1 / y_sin) * x_cos; // x scale to move 1 y
+	double y_scale = (1 / x_cos) * y_sin; // y scale to move 1 x
 
-	// printf("xDiff=%f yDiff=%f\n", xDiff, yDiff);
-	// printf("xRatio=%f yRatio=%f\n", xRatio, yRatio);
-    double x = playerX;
-    double y = playerY;
-	double length = 0;
+	// printf("x_cos=%f y_sin=%f\n", x_cos, y_sin);
+	// printf("x_scale=%f y_scale=%f\n", x_scale, y_scale);
 
-    double x2 = playerX;
-    double y2 = playerY;
+    double x_horizontal = playerX;
+    double y_horizontal = playerY;
+	double horizontal_length = 0;
+
+	double x_vertical = playerX;
+	double y_vertical = playerY;
+	double vertical_length = 0;
+
 	int quartile;
-
-	double length2 = 0;
+	// GET QUARTILE
 	if (angle >= 0 && angle <= 90)
-	{
-		// printf("%f bas droite x++ y++\n", angle);
 		quartile = 1;
-	}
 	else if (angle >= 91 && angle <= 180)
-	{
-		// printf("%f bas gauche x-- y++\n", angle);
 		quartile = 2;
-	}
 	else if (angle >= 181 && angle <= 270)
-	{
-		// printf("%f haut gauche x-- y--\n", angle);
 		quartile = 3;
-	}
 	else if (angle >= 271 && angle <= 360)
-	{
-		// printf("%f haut droite x++ y--\n", angle);
 		quartile = 4;
+	// VERTICAL CHECK
+    while (x_vertical >= 0 && x_vertical < MAP_WIDTH && y_vertical >= 0 && y_vertical < MAP_HEIGHT)
+    {
+		if (check_map_vertical(x_vertical, y_vertical, quartile))
+			break ;
+		get_next_vertical_intersec(&x_vertical, &y_vertical, y_scale, quartile);
+    }
+	vertical_length = fabs(x_vertical - playerX) * fabs(x_cos) + fabs(y_vertical - playerY) * fabs(y_sin);
+	// HORIZONTAL CHECK
+	while (x_horizontal >= 0 && x_horizontal < MAP_WIDTH && y_horizontal >= 0 && y_horizontal < MAP_HEIGHT)
+	{
+		if (check_map_horizontal(x_horizontal, y_horizontal, quartile))
+			break ;
+		get_next_horizontal_intersec(&x_horizontal, &y_horizontal, x_scale, quartile);
 	}
-    while (x >= 0 && x < MAP_WIDTH && y >= 0 && y < MAP_HEIGHT)
-    {
-			get_next1_point(&x, &y, yRatio, quartile);
-			if (x < 0 || x > MAP_WIDTH || y < 0 || y > MAP_HEIGHT)
-				break ;
-			if (quartile == 1)
-			{
-				if (map[(int)y][(int)x] == 1)
-					break ;
-			}
-			else if (quartile == 2)
-			{
-				if (map[(int)y][(int)x - 1] == 1)
-					break ;
-			}
-			else if (quartile == 3)
-			{
-				// printf("FOUND AT %d, %d or %f, %f\n", (int)x-1, (int)y, x - 1, y);
-				if (map[(int)y][(int)x - 1] == 1)
-					break ;
-			}
-			else if (quartile == 4)
-			{
-				if (map[(int)y][(int)x] == 1)
-					break ;
-			}
-    }
-	length = sqrt((y - playerY) * (y - playerY) + (x - playerY) * (x - playerY));
-	// printf("length1=%f at: %f, %f\n", length, x, y);
-    while (x2 >= 0 && x2 < MAP_WIDTH && y2 >= 0 && y2 < MAP_HEIGHT)
-    {
-			get_next2_point(&x2, &y2, xRatio, quartile);
-			if (x2 < 0 || x2 > MAP_WIDTH || y2 < 0 || y2 > MAP_HEIGHT)
-				break ;
-			if (quartile == 1)
-			{
-				if (map[(int)y2][(int)x2] == 1)
-					break ;
-			}
-			else if (quartile == 2)
-			{
-				// printf("ici--------------------\n");
-				if (map[(int)y2][(int)x2] == 1)
-					break ;
-			}
-			else if (quartile == 3)
-			{
-				// xDiff = ceil(xDiff);
-				// yDiff = ceil(yDiff);
-				// printf("FOUND AT %d, %d or %f, %f\n", (int)ceil(x2) - 1, (int)y2 - 1, x2, y2);
-				if (map[(int)y2 - 1][(int)ceil(x2) - 1] == 1)
-					break ;
-			}
-			else if (quartile == 4)
-			{
-				if (map[(int)y2 - 1][(int)x2] == 1)
-					break ;
-			}
-    }
-	// printf("wsh\n");
-	length2 = sqrt((y2 - playerY) * (y2 - playerY) + (x2 - playerY) * (x2 - playerY));
-	// printf("length2=%f at: %f, %f\n", length2, x2, y2);
-	if (length > length2)
-		return (printf("length2=%f at: %f, %f\n", length2, x2, y2), length2);
+	horizontal_length = fabs(x_horizontal - playerX) * fabs(x_cos) + fabs(y_horizontal - playerY) * fabs(y_sin);
+	// GET BEST LENGTH
+	if (vertical_length > horizontal_length)
+	{
+		 printf("horizontal_length=%f at: %f, %f\n", horizontal_length, x_horizontal, y_horizontal);
+		return (horizontal_length);
+	}
 	else
-		return (printf("length=%f at: %f, %f\n", length, x, y), length);
+	{
+		 printf("vertical_length=%f at: %f, %f\n", vertical_length, x_vertical, y_vertical);
+		return (vertical_length);
+	}
 }
 
 int main(void)
 {
-    double playerX = 5.0;
-    double playerY = 5.0;
-	double angle = 231;
+    double playerX = 3;
+    double playerY = 2;
+	double angle = 0;
    
     // for (double angle = 0; angle < 60; angle += 0.06) {
-		// get_vector_distance(playerX, playerY, angle);
+		 get_vector_distance(playerX, playerY, angle);
     // }
-	ft_ceil(1.654);
     return 0;
 }
